bf4c474351ec2278cbb51cd81d1baf2f72777291
Jose Jorge Rodriguez Salgado <josejorgexl@gmail.com>
2020-07-27 18:07:32 -0700

    All properties initialized inside Requester ctor. Typo in the name of the package. Is 'threaded-requester' instead of 'threaded-request'

--- threaded-requester.js
+++ threaded-requester.js
@@ -2,11 +2,6 @@
 
 export default class Requester{
 
-    opts;
-    url;
-    stop;
-    timeout;
-
     constructor(url, opts=null){
         this.url = url
         this.opts = opts
@@ -15,46 +10,45 @@ export default class Requester{
             this.timeout = this.opts.timeout
         else
             this.timeout = 60000;
+        
+        this.configBuilder = () => {
+
+            let settings = {
+                method: 'GET'
+            };
+    
+            if(this.opts.method)
+                settings.method = this.opts.method
+            if(this.opts.headers)
+                settings.headers = this.opts.headers
+            if(this.opts.body)
+                settings.body = this.opts.body
+    
+            return settings
+        }
+
+        this.run = () => {
+            this.stop = false;
+            this.runThread();
+        }
+
+        this.runThread = () => {
+    
+            if(this.stop)
+                return;
+            const settings = this.configBuilder();
+            setTimeout(() => fetch(this.url, settings)
+                                .then(result => this.opts.callback ? this.opts.callback(result) : null)
+                                .then(() => this.runThread())
+                                .catch(result => {
+                                    if(this.opts.failure)
+                                        this.opts.failure(result)
+                                    this.runThread()
+                                }), this.timeout)
+        }
+
+        this.stop = () => {
+            this.stop = true;
+        }
     }
-
-    configBuilder = () => {
-
-        let settings = {
-            method: 'GET'
-        };
-
-        if(this.opts.method)
-            settings.method = this.opts.method
-        if(this.opts.headers)
-            settings.headers = this.opts.headers
-        if(this.opts.body)
-            settings.body = this.opts.body
-
-        return settings
-    } 
-
-
-    run(){
-        this.stop = false;
-        this.runThread();
-    }
-    runThread(){
-
-        if(this.stop)
-            return;
-        const settings = this.configBuilder();
-        setTimeout(() => fetch(this.url, settings)
-                            .then(result => this.opts.callback ? this.opts.callback(result) : null)
-                            .then(() => this.runThread())
-                            .catch(result => {
-                                if(this.opts.failure)
-                                    this.opts.failure(result)
-                                this.runThread()
-                            }), this.timeout)
-    }
-
-    stop(){
-        this.stop = true;
-    }
-
 }
